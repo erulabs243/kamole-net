8e000e7fdccddf5f498ef31e007788cca7d1b26a
Glorieux Lukama <ir.antoineglorieux@gmail.com>
2025-07-09 13:04:54 +0200

    feat: replace brand font by IBM Plex

--- src/lib/components/partials/header.svelte
+++ src/lib/components/partials/header.svelte
@@ -1,19 +1,69 @@
 <script lang="ts">
-	import type { Category } from '@/data/gql';
-	import { stack } from 'styled-system/patterns';
+import { MenuIcon, SearchIcon } from "lucide-svelte";
 
-	import { buttonRecipe } from '$lib/components/ui/button';
+import type { Category } from "@/data/gql";
+import { stack } from "styled-system/patterns";
+import { css, cx } from "styled-system/css";
 
-	interface Props {
-		categories: Array<Category>;
-	}
+import { buttonRecipe } from "$lib/components/ui/button";
 
-	let { categories }: Props = $props();
+interface Props {
+	categories: Array<Category>;
+}
+
+let { categories }: Props = $props();
 </script>
 
 <header>
 	<!-- Title with sidebar -->
-	<div></div>
+	<div class={stack({
+			px: { base: "4", sm: "8"},
+			py: "2",
+			flexDir: "row",
+			alignItems: "center",
+			justify: "space-between"
+		})}>
+		<!-- Drawer -->
+		<div class={stack({
+				flexDir: "row",
+				gap: "2"
+			})}>
+			<a
+				href="/search"
+				class={css({
+					display: "inline-flex",
+					p: "2",
+					rounded: "sm",
+					hideBelow: "sm",
+					_hover: { bg: "bg.subtle"}
+				})}
+			>
+				<SearchIcon class={css({ w: "6", h: "6", color: "fg" })} />
+			</a>
+		</div>
+
+		<!-- Logo -->
+		<p>Logo</p>
+
+		<!-- Right navigation -->
+		<div class={stack({
+					flexDir: "row",
+				})}>
+
+			<a
+				href="/search"
+				class={css({
+					display: "inline-flex",
+					p: "2",
+					rounded: "sm",
+					hideFrom: "sm",
+					_hover: { bg: "bg.subtle"}
+				})}
+			>
+				<SearchIcon class={css({ w: "6", h: "6", color: "fg" })} />
+			</a>
+		</div>
+	</div>
 	<!-- Navigation -->
 	<nav
 		class={stack({
--- src/lib/components/ui/text-styles.ts
+++ src/lib/components/ui/text-styles.ts
@@ -1,11 +1,32 @@
-import { defineTextStyles } from '@pandacss/dev';
+import { defineTextStyles } from "@pandacss/dev";
 
 export const textStyles = defineTextStyles({
-	h1: { value: { fontSize: '5xl', fontWeight: 'bold', lineHeight: '1.2' } },
-	h2: { value: { fontSize: '4xl', fontWeight: 'bold', lineHeight: '1.1' } },
-	h3: { value: { fontSize: '3xl', fontWeight: 'bold', lineHeight: '1.1' } },
-	h4: { value: { fontSize: '2xl' } },
-	h5: { value: { fontSize: 'xl' } },
-	h6: { value: { fontSize: 'lg' } },
-	paragraph: { value: { fontSize: 'md' } }
+	h1: {
+		value: {
+			fontSize: "5xl",
+			fontWeight: "bold",
+			lineHeight: "1.2",
+			fontFamily: "ibmSerif",
+		},
+	},
+	h2: {
+		value: {
+			fontSize: "4xl",
+			fontWeight: "bold",
+			lineHeight: "1.1",
+			fontFamily: "ibmSerif",
+		},
+	},
+	h3: {
+		value: {
+			fontSize: "3xl",
+			fontWeight: "bold",
+			lineHeight: "1.1",
+			fontFamily: "ibmSerif",
+		},
+	},
+	h4: { value: { fontSize: "2xl", fontFamily: "ibmSerif" } },
+	h5: { value: { fontSize: "xl", fontFamily: "ibmSerif" } },
+	h6: { value: { fontSize: "lg" } },
+	paragraph: { value: { fontSize: "md" } },
 });
